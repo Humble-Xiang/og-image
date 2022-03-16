02699314f6b28e26eb19388e733cc94122ed54b8
Humble.X <Humble.Xiang@gmail.com>
2022-03-16 09:07:06 +0800

    refactor: 添加中文字体、自定义默认图片、自定义模版部分样式

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,9 +7,11 @@ const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
+const recursive1 = readFileSync(`${__dirname}/../_fonts/Recursive01.woff`).toString('base64');
+const recursive2 = readFileSync(`${__dirname}/../_fonts/Recursive02.woff`).toString('base64');
+const recursive3 = readFileSync(`${__dirname}/../_fonts/Recursive03.woff`).toString('base64');
+const recursive4 = readFileSync(`${__dirname}/../_fonts/Recursive04.woff`).toString('base64');
+const ceyy = readFileSync(`${__dirname}/../_fonts/仓耳渔阳体W02.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
     let background = 'white';
@@ -22,26 +24,49 @@ function getCss(theme: string, fontSize: string) {
         radial = 'dimgray';
     }
     return `
+    /* cyrillic-ext */
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
+        font-family: 'Recursive';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: url(data:font/woff;charset=utf-8;base64,${recursive1})  format('woff');
+        unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
     }
-
+    /* vietnamese */
     @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
+        font-family: 'Recursive';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: url(data:font/woff;charset=utf-8;base64,${recursive2})  format('woff');
+        unicode-range: U+0102-0103, U+0110-0111, U+0128-0129, U+0168-0169, U+01A0-01A1, U+01AF-01B0, U+1EA0-1EF9, U+20AB;
     }
-
+    /* latin-ext */
     @font-face {
-        font-family: 'Vera';
+        font-family: 'Recursive';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: url(data:font/woff;charset=utf-8;base64,${recursive3})  format('woff');
+        unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+    }
+    /* latin */
+    @font-face {
+        font-family: 'Recursive';
+        font-style: normal;
+        font-weight: 400;
+        font-display: swap;
+        src: url(data:font/woff;charset=utf-8;base64,${recursive4})  format('woff');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
+    /* 中文 */
+    @font-face {
+        font-family: 'CEYY';
         font-style: normal;
         font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
+        src: url(data:font/woff2;charset=utf-8;base64,${ceyy})  format("woff2");
+    }
 
     body {
         background: ${background};
@@ -56,7 +81,7 @@ function getCss(theme: string, fontSize: string) {
 
     code {
         color: #D400FF;
-        font-family: 'Vera';
+        font-family: 'Recursive', 'CEYY';
         white-space: pre-wrap;
         letter-spacing: -5px;
     }
@@ -79,12 +104,12 @@ function getCss(theme: string, fontSize: string) {
 
     .plus {
         color: #BBB;
-        font-family: Times New Roman, Verdana;
+        font-family: 'Recursive', 'CEYY';
         font-size: 100px;
     }
 
     .spacer {
-        margin: 150px;
+        margin: 50px;
     }
 
     .emoji {
@@ -95,7 +120,7 @@ function getCss(theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter', sans-serif;
+        font-family: 'Recursive', 'CEYY';
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: ${foreground};
@@ -131,7 +156,7 @@ export function getHtml(parsedReq: ParsedRequest) {
 </html>`;
 }
 
-function getImage(src: string, width ='auto', height = '225') {
+function getImage(src: string, width ='auto', height = '400') {
     return `<img
         class="logo"
         alt="Generated Image"
